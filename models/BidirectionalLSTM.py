936c6c4f7ba601c8a1fc1127e81a96af2b3b7b35
goldentimecoolk <39372277+goldentimecoolk@users.noreply.github.com>
2018-10-09 12:12:41 +0800

    Update BidirectionalLSTM.py

--- models/BidirectionalLSTM.py
+++ models/BidirectionalLSTM.py
@@ -36,7 +36,7 @@ class BidirectionalLSTM(nn.Module):
         dropout: If non-zero, introduces a dropout layer on the outputs of each RNN layer except the last layer
         bidirectional: If True, becomes a bidirectional RNN. Default: False
         '''
-        self.lstm = nn.LSTM(input_size=self.vector_dim,
+        self.lstm = nn.LSTM(input_size=self.vector_dim,                     ### is n_classes
                             num_layers=self.num_layers,
                             hidden_size=self.hidden_size,
                             bidirectional=True)
